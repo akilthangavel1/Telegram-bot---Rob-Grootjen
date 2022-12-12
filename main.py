d3ecc132faa1fc784a5d7492f9b8137108914a3d
Akil <akilthangavel@gmail.com>
2022-12-13 00:54:00 +0530

    update url

--- main.py
+++ main.py
@@ -170,7 +170,7 @@ def main() -> None:
     application.add_handler(CommandHandler("addchannel", add_channel_command))
     application.run_webhook(listen="0.0.0.0",
                           port=int(PORT),
-                          url_path="5860704176:AAHYGwwnky3kPmsYW44TpHleMv6BqbDRN3U")
+                          url_path='https://mighty-everglades-75025.herokuapp.com/' + "5860704176:AAHYGwwnky3kPmsYW44TpHleMv6BqbDRN3U")
     application.bot.set_webhook('https://mighty-everglades-75025.herokuapp.com/' + "5860704176:AAHYGwwnky3kPmsYW44TpHleMv6BqbDRN3U")
     application.idle()
 
